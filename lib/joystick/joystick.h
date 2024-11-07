bdc9af8bc5c93c1cfe7328fe9ee0114785e17eae
Sifodya <cedric.schlegel@bluewin.ch>
2024-11-07 10:54:51 +0100

    Commented duplicate vars

--- lib/joystick/joystick.h
+++ lib/joystick/joystick.h
@@ -7,9 +7,9 @@ class Joystick
     #define DELTA_THRESHOLD 3 //threshold to output midi data
     #define MAX_ADC 1023 //max value read by adc
     #define ALPHA 0.9 // smoothing factor (0.0 to 1.0)
-    int valueX, valueY = 0;
-    int smoothedX, smoothedY = 0;
-    int deltaX, deltaY = 0;
+    //int valueX, valueY = 0;
+    //int smoothedX, smoothedY = 0;
+    //int deltaX, deltaY = 0;
     //int scaleValueX, scaleValueY = 0;
     public:
     void updatedDisplay(int joystickvalueX, int joystickValueY);
